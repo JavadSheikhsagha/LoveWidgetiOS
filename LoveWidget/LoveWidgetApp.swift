6c96dbe61851e1a60df73c4ede027515f5bb27be
Javad <sir.hxt@gmail.com>
2023-08-29 10:43:32 +0330

    Initial Commit

---
+++ LoveWidget/LoveWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveWidgetApp.swift
+//  LoveWidget
+//
+//  Created by enigma 1 on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LoveWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
