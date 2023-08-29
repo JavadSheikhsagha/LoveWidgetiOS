e5f82c5c2bd09b658626ce0c188616237ff35b50
Javad <sir.hxt@gmail.com>
2023-08-29 11:29:49 +0330

    [FEATURE] implement Login Screen view regex

--- LoveWidget/login/LoginScreen.swift
+++ LoveWidget/login/LoginScreen.swift
@@ -45,6 +45,14 @@ struct LoginScreen: View {
                                 
                             }.textContentType(.emailAddress).padding()
                                 .frame(width: UIScreen.main.bounds.width - 64, height: 55)
+                                .onChange(of: email) { newValue in
+                                    if textFieldValidatorEmail(newValue) && passwordText.isValidPassword() {
+                                        isButtonEnabled = true
+                                    } else {
+                                        isButtonEnabled = false
+                                    }
+                                }
+                                
                         }.cornerRadius(10) /// make the background rounded
                         .overlay( /// apply a rounded border
                             RoundedRectangle(cornerRadius: 10)
@@ -53,11 +61,19 @@ struct LoginScreen: View {
                         
                         
                         
+                        
                         ZStack {
                             
                             SecureField ("Password", text: $passwordText)
                                 .padding()
                                 .frame(width: UIScreen.main.bounds.width - 64, height: 55)
+                                .onChange(of: passwordText) { newValue in
+                                    if textFieldValidatorEmail(email) && newValue.isValidPassword() {
+                                        isButtonEnabled = true
+                                    } else {
+                                        isButtonEnabled = false
+                                    }
+                                }
                             
                         }.cornerRadius(10) /// make the background rounded
                         .overlay( /// apply a rounded border
@@ -107,6 +123,27 @@ struct LoginScreen: View {
             
             
         }
+    }
+    
+
+    
+    func textFieldValidatorEmail(_ string: String) -> Bool {
+        if string.count > 100 {
+            return false
+        }
+        let emailFormat = "(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}" + "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" + "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-" + "z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5" + "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" + "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" + "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        //let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: string)
+    }
+    
+}
+
+extension String {
+    
+    public func isValidPassword() -> Bool {
+        let passwordRegex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*()\\-_=+{}|?>.<,:;~`’]{8,}$"
+        return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
     }
 }
 
