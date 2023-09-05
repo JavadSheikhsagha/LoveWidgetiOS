0445267701bf5dc0b708458191abd45042dfb0cd
Javad <sir.hxt@gmail.com>
2023-09-05 19:07:51 +0330

    [FEATURE] implement firebase

--- LoveWidget/app/LoveWidgetApp.swift
+++ LoveWidget/app/LoveWidgetApp.swift
@@ -5,10 +5,21 @@
 //  Created by enigma 1 on 8/29/23.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct LoveWidgetApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     @StateObject var mainViewModel = MainViewModel()
     @StateObject var friendsViewModel = FriendsViewModel()
