e0b76368722c230488f57ffb370bd308bd9a6f8d
Javad <sir.hxt@gmail.com>
2023-10-16 15:00:59 +0330

    [FEATURE] implement draw screen

---
+++ LoveWidget/presentation/widget/DrawScreen.swift
@@ -0,0 +1,18 @@
+//
+//  DrawScreen.swift
+//  LoveWidget
+//
+//  Created by Javad on 10/16/23.
+//
+
+import SwiftUI
+
+struct DrawScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawScreen()
+}
